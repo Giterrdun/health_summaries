db8897cc344437bede8d9917f3a6dbf8d89e3f2e
unknown <rodriguezmelojp@MVHS-CS15-20.dcsdk12.org>
2015-09-22 10:21:44 -0600

    data.cvs split into array

--- health_summaries.rb
+++ health_summaries.rb
@@ -1,25 +1,30 @@
+
+
 module HealthSummaries
-# file = File.open("data.csv", 'r')
+file = File.open("data.csv", 'r')
+file.each_line do |line|
+	array = line.split(',')
 
-	def max(file)
-		file.max
-	end
-
-	def min(file)
-		file.min  
-	end
-
-	def mean(array)
-		y = 0
-		for x in 0...array.length	
-			y += array[x]
+		def max(array)
+			return array.max
 		end
-		y / array.length 
-	end
+
+		def min(array)
+			return array.min  
+		end
+
+		def mean(array)
+			y = 0
+			for x in 0...array.length	
+				y += array[x]
+			end
+			y / array.length 
+		end
+		
 
 
+end
 
 
  
 end
-
